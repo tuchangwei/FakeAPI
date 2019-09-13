81419575c64df472c26a5396e6ca914c82604fa2
Changwei Tu <changweitu@gmail.com>
2019-09-13 08:49:00 +0800

    add fake api for login

--- Sources/App/Controllers/LoginController.swift
+++ Sources/App/Controllers/LoginController.swift
@@ -2,18 +2,27 @@ import Vapor
 
 /// Controls basic CRUD operations on `Todo`s.
 final class LoginController: RouteCollection {
+    struct LoginParams: Content {
+        let email: String
+        let password: String
+    }
+    struct LoginResult: Content {
+        let token: String?
+        let error: String?
+    }
     func boot(router: Router) throws {
         let loginRouter = router.grouped("api", "login")
-        loginRouter.post { request -> Future<HTTPStatus> in
-            return try request.content.decode(User.self)
-                .map(to: HTTPStatus.self) { user in
-                    print("\(user.email), \(user.password)")
-                    if user.email == "1@1.com" && user.password == "123456" {
-                        return .ok
-                    } else {
-                        return .notAcceptable
-                    }
-            }
+        loginRouter.post { request -> Future<Response> in
+            return try request.content.decode(LoginParams.self).flatMap({ user in
+                if user.email == "1@1.com" && user.password == "123456" {
+                    return LoginResult(token: String(Date().timeIntervalSince1970), error: nil )
+                        .encode(status: .ok, for: request)
+                } else {
+                    return LoginResult(token: nil, error: "Email or Password is incorrect" )
+                        .encode(status: .unauthorized, for: request)
+                }
+                
+            })
         }
     }
 }
--- Sources/App/Models/User.swift
+++
@@ -1,7 +0,0 @@
-import FluentSQLite
-import Vapor
-
-struct User: Content {
-    let email: String
-    let password: String
-}
